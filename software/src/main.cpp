d2b6378d0dff2d5333db49f70b9b3a9289fc6986
Guilhelm Savin <guilhelm.savin@gmail.com>
2015-08-13 16:39:03 +0200

    Add XBee initialization code.

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -22,6 +22,8 @@
   #include "Arduino.h"
 #endif
 
+#include <XBee.h>
+
 #define HLED 3
 #define LLED 5
 #define CLED 13
@@ -29,12 +31,63 @@
 #define A_WRITE(x, d) { analogWrite(HLED, x);  analogWrite(LLED, x);  delay(d); }
 #define D_WRITE(x, d) { digitalWrite(HLED, x); digitalWrite(LLED, x); delay(d); }
 
+#define BLINK(L, D, C) for (int blinki = 0; blinki < C; blinki++) \
+  { digitalWrite(L, HIGH); delay(D); digitalWrite(L, LOW); delay(D); }
+
+XBee xbee;
+
+void xbee_magic()
+{
+  char thisByte = 0;
+
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+	Serial.print("+++");
+
+	while (thisByte != '\r' && thisByte != '\n') {
+		if (Serial.available() > 0)
+			thisByte = Serial.read();
+	}
+
+  //
+  // Here starts the magic ;)
+  // Well, actually it's just some AT commands to set up the network.
+  //
+
+  Serial.print("ATRE\r");
+	Serial.print("ATAP2\r");
+	Serial.print("ATCE");
+	Serial.print(XBEE_COORDINATOR_ENABLE);
+	Serial.print("\r");
+	Serial.print("ATMY");
+	Serial.print(XBEE_ADDRESS);
+	Serial.print("\r");
+	Serial.print("ATID");
+	Serial.print(XBEE_NETWORK);
+	Serial.print("\r");
+	Serial.print("ATCH0C\r");
+	Serial.print("ATCN\r");
+}
+
 void setup()
 {
   pinMode(CLED, OUTPUT);
 
+  BLINK(CLED, 150, 5);
+
   pinMode(HLED, OUTPUT);
   pinMode(LLED, OUTPUT);
+
+  //
+  // XBee initialization
+  //
+
+  digitalWrite(CLED, LOW);
+
+  xbee = XBee();
+  xbee_magic();
+
+  digitalWrite(CLED, HIGH);
 }
 
 void loop()
